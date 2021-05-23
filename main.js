12356db084a5a9e6435d65612edefb903f4063d5
Aarav-is-EPIC <76231563+Aarav-is-EPIC@users.noreply.github.com>
2021-05-23 16:29:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+perdiction1="";
+perdiction2="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+
+});
+
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='image_captured' src ='" + data_uri + "'>"
+    });
+}
+console.log("ml5 version:",ml5.version);
